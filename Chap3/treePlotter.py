0a8108ee20a913ef70a2b7dea30970481dd8a186
vipul <letsmailvipul@gmail.com>
2018-05-15 16:00:33 +0530

    .

--- Chap3/treePlotter.py
+++ Chap3/treePlotter.py
@@ -8,16 +8,38 @@ leafNode = dict(boxstyle='round4', fc='0.8')
 arrow_args = dict(arrowstyle='<-')
 
 def plotMidText(cntrPt, parentPt, txtString):
+	'''
+	Plot text between child and parent
+	'''
 	xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
 	yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
 	createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+	numLeafs = getNumLeafs(myTree)
+	maxDepth = getTreeDepth(myTree)
+	firstStr = myTree.keys()[0]
+	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)/2.0/plotTree.totalW, plotTree.yOff)
+	plotMidText(cntrPt, parentPt, nodeTxt)
+	plotNode(firstStr, cntrPt, parentPt, decisionNode)
+	secondDict = myTree[firstStr]
+	plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__ == 'dict':
+			plotTree(secondDict[key], cntrPt, str[key])
+		else:
+			plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+			plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+			plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+	plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+	
 
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
 	createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',\
 							xytext=centerPt, textcoords='axes fraction', \
 							va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
 	
-def createPlot():
+def createPlot(inTree):
 	fig = plt.figure(1, facecolor='white')
 	fig.clf()
 	createPlot.ax1 = plt.subplot(111, frameon=False)
