2d7448522dc9db1df7e316b4ea0bc79ddffb4acc
vipul <letsmailvipul@gmail.com>
2018-05-22 04:28:33 +0530

    .

--- Chap4/Bayes.py
+++ Chap4/Bayes.py
@@ -23,3 +23,11 @@ def createVocabList(dataSet):
 		vocabSet = vocabSet | set(document)	# Creates the union of two set
 	return list(vocabSet)
 	
+def setOfWords2Vec(vocabList, inputSet):
+	returnVec = [0] * len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocablist.index(word)] = 1
+		else:
+			print('The word: %s is not in my Vocabulary!' % word)
+	returnVec
