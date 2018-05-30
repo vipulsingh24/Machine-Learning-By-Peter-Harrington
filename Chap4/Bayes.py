1c02345e08270266103ac56a773819090e056f2c
vipul <letsmailvipul@gmail.com>
2018-05-30 21:17:48 +0530

    .

--- Chap4/Bayes.py
+++ Chap4/Bayes.py
@@ -1,9 +1,12 @@
 '''
 Naive Bayes Classifier
 '''
-
+from numpy import *
 
 def loadDataSet():
+	'''
+	Example data to experiment with.
+	'''
 	postingList = [['my', 'dog', 'has', 'flea',\
 					'problems', 'help', 'please'],
 					['maybe', 'not', 'take', 'him',\
@@ -18,12 +21,19 @@ def loadDataSet():
 	return postingList, classVec
 
 def createVocabList(dataSet):
+	'''
+	Creates a list of all the unique words in all of our documents.
+	'''
 	vocabSet = set([])
 	for document in dataSet:
 		vocabSet = vocabSet | set(document)	# Creates the union of two set
 	return list(vocabSet)
 	
 def setOfWords2Vec(vocabList, inputSet):
+	'''
+	Outputs a vector of 0s and 1s to represent whether a word from vocabulary list
+	is present or not in the given document.
+	'''
 	returnVec = [0] * len(vocabList)
 	for word in inputSet:
 		if word in vocabList:
