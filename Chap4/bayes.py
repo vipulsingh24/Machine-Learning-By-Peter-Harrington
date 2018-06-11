06e2c712b423a8644fcdf59bd8763b528c5e0e73
vipul <letsmailvipul@gmail.com>
2018-06-12 01:39:36 +0530

    .

--- Chap4/bayes.py
+++ Chap4/bayes.py
@@ -42,6 +42,14 @@ def setOfWords2Vec(vocabList, inputSet):
 			print('The word: %s is not in my Vocabulary!' % word)
 	return returnVec
 
+def bagOfWords2VecMN(vocabList, inputSet):
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] += 1
+	return returnVec
+	
+
 def trainNB0(trainMatrix, trainCategory):
 	numTrainDocs = len(trainMatrix)
 	numWords = len(trainMatrix[0])
@@ -78,7 +86,42 @@ def testingNB():
 	p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
 	testEntry = ['love', 'my', 'dalmation']
 	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-	print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+	print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
 	testEntry = ['stupid', 'garbage']
 	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-	print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+	print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+	
+def spamTest():
+    docList=[]; classList = []; fullText =[]
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)#create vocabulary
+    trainingSet = range(50); testSet=[]           #create test set
+    for i in range(10):
+        randIndex = int(random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])  
+    trainMat=[]; trainClasses = []
+    for docIndex in trainingSet:#train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:        #classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+            errorCount += 1
+            print("Classification error",docList[docIndex])
+    print('The error rate is: ',float(errorCount)/len(testSet))
